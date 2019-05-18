994d4935782a64f69d6cdfc1305baecd8b2095df
Pwootage <pwootage@gmail.com>
2019-05-18 14:44:02 -0600

    Add area id parsing

--- main.cpp
+++ main.cpp
@@ -14,8 +14,15 @@ using namespace nlohmann;
 char *memory;
 size_t memory_size;
 
+void readObjectList();
+
+void testRandom();
+
+void readAreas();
+
 int main() {
-  FILE *f = fopen("ram.raw", "rb");
+//  FILE *f = fopen("ram.raw", "rb");
+  FILE *f = fopen("ram_borked79.raw", "rb");
   fseek(f, 0, SEEK_END);
   memory_size = static_cast<size_t>(ftell(f));
   fseek(f, 0, SEEK_SET);
@@ -23,50 +30,9 @@ int main() {
   memory = new char[memory_size];
   fread(memory, 1, memory_size, f);
 
-  CStateManager manager(CStateManager::LOCATION);
-//  cout << hex << manager.player.deref().ptr() << dec << endl;
-//  cout << hex << manager.random.seed.read() << endl;
-
-//  CRandom16Impl impl(manager.random.seed.read());
-//  CRandom16Impl impl(0x91F76E96);
-//
-//  for (int i = 0; i < 100; i++) {
-//    cout << hex << impl.GetSeed() << dec << " ";
-//    cout << impl.Float() << endl;
-//  }
-
-  CObjectList list = manager.allObjs.deref();
-
-  cout << "Size: " << list.count.read() << endl;
-  int counter = 0;
-  int id = list.firstID.read();
-  cout << hex;
-  while (id != 0xFFFF) {
-    counter++;
-    SObjectListEntry entry = list.list[id & 0x3FF];
-
-    CEntity entity = entry.entity.deref();
-//    if (entity.editorID.read() == 0x340286) {
-      cout << "ID: " << entity.editorID.read() << endl;
-      cout << "Address: " << entity.editorID.ptr() << endl;
-      cout << "Name loc: " << entity.name.ptr() << endl;
-      cout << "Name string ptr: " << entity.name.data.read() << endl;
-      cout << "Name: " << entity.name.read_str() << endl;
-      cout << "Unknowns: " << entity.name.unk2.read() << " | " << entity.name.unk3.read() << " | "<< entity.name.unk4.read() << endl;
-//      cout << "State flag: " << (uint32_t)entity.status.read() << endl;
-//
-//      cout << "State flag address: " << entity.status.ptr() << endl;
-    cout << "------------------------------" << endl << endl;
-//    }
-    int newId = entry.next.read();
-    if (id == newId) break;
-    id = newId;
-    if (counter > 0x3FF) break; //somethign went wrong
-  }
-  if (counter != list.count.read()) {
-    cout << "Something is very wrong" << endl;
-  }
-  cout << dec;
+  readAreas();
+  //  testRandom();
+//  readObjectList();
 
 
 //  cout << Prime1JsonDumper::parseHeap().dump(2) << endl;
@@ -93,4 +59,140 @@ int main() {
 //  cout << json_message.dump(2) << endl;
 
   return 0;
+}
+
+void readAreas() {
+  CStateManager manager(CStateManager::LOCATION);
+
+  CWorld world = manager.world.deref();
+  int areaCount = world.areas.size.read();
+
+  for (int i = 0; i < areaCount; i++) {
+    auto autoPtr = world.areas[i];
+//    cout << hex << " autoptr offset: " << autoPtr.ptr();
+//    cout << hex << " autoptr second value: " << autoPtr.dataPtr.read();
+//    cout << endl;
+    CGameArea area = autoPtr.dataPtr.deref();
+    cout << hex << "Area " << area.selfIDX.read() << "|" << area.mrea.read() << endl;
+    cout << (area.postConstructedBool.read() ? "C" : " ");
+    cout << (area.active.read() ? "A" : " ");
+    cout << (area.tokensReady.read() ? "T" : " ");
+    cout << (area.loadPaused.read() ? "P" : " ");
+    cout << (area.validated.read() ? "V" : " ");
+    cout << " phase: ";
+    switch (area.phase.read()) {
+      case EPhase::LoadHeader:
+        cout << "Load header";
+        break;
+      case EPhase::LoadSecSizes:
+        cout << "Load section sizes";
+        break;
+      case EPhase::ReserveSections:
+        cout << "Reserve Sections";
+        break;
+      case EPhase::LoadDataSections:
+        cout << "Load data sections";
+        break;
+      case EPhase::WaitForFinish:
+        cout << "Wait for finish";
+        break;
+      default:
+        cout << "Unknown";
+    }
+    cout << " chain: ";
+    switch (area.curChain.read()) {
+      case EChain::Invalid:
+        cout << "Invalid";
+        break;
+      case EChain::ToDeallocate:
+        cout << "ToDeallocate";
+        break;
+      case EChain::Deallocated:
+        cout << "Deallocated";
+        break;
+      case EChain::Loading:
+        cout << "Loading";
+        break;
+      case EChain::Alive:
+        cout << "Alive";
+        break;
+      case EChain::AliveJudgement:
+        cout << "Alive Judgement";
+        break;
+      default:
+        cout << "Unknown";
+    }
+
+    if (area.postConstructed.read() != 0) {
+      CPostConstructed pc = area.postConstructed.read();
+      cout << " Occlusion state: ";
+      switch (pc.occlusionState.read()) {
+        case EOcclusionState::Occluded:
+          cout << "Occluded";
+          break;
+        case EOcclusionState::Visible:
+          cout << "Visible";
+          break;
+        default:
+          cout << "Unknown";
+      }
+    }
+
+    cout << endl;
+
+
+//    cout << "Post constructed auto_ptr: " << (int) area.postConstructed.unknown.read() << " | " << area.postConstructed.dataPtr.read() << endl;
+
+    cout << "Load transaction offset: " << area.loadTransactions.ptr() << endl;
+
+  }
+}
+
+void testRandom() {
+//  CStateManager manager(CStateManager::LOCATION);
+//  CRandom16Impl impl(manager.random.seed.read());
+  CRandom16Impl impl(0x91F76E96);
+
+  for (int i = 0; i < 100; i++) {
+    cout << hex << impl.GetSeed() << dec << " ";
+    cout << impl.Float() << endl;
+  }
+}
+
+void readObjectList() {
+  CStateManager manager(CStateManager::LOCATION);
+
+  CObjectList list = manager.allObjs.deref();
+
+  cout << "Size: " << list.count.read() << endl;
+  int counter = 0;
+  int id = list.firstID.read();
+  cout << hex;
+  while (id != 0xFFFF) {
+    counter++;
+    SObjectListEntry entry = list.list[id & 0x3FF];
+
+    CEntity entity = entry.entity.deref();
+//    if (entity.editorID.read() == 0x340286) {
+    cout << "ID: " << entity.editorID.read() << endl;
+    cout << "Address: " << entity.editorID.ptr() << endl;
+    cout << "Name loc: " << entity.name.ptr() << endl;
+    cout << "Name string ptr: " << entity.name.data.read() << endl;
+    cout << "Name: " << entity.name.read_str() << endl;
+    cout << "Unknowns: " << entity.name.unk2.read() << " | " << entity.name.unk3.read() << " | "
+         << entity.name.unk4.read() << endl;
+//      cout << "State flag: " << (uint32_t)entity.status.read() << endl;
+//
+//      cout << "State flag address: " << entity.status.ptr() << endl;
+    cout << "------------------------------" << endl << endl;
+//    }
+    int newId = entry.next.read();
+    if (id == newId) break;
+    id = newId;
+    if (counter > 0x3FF) break; //somethign went wrong
+  }
+  if (counter != list.count.read()) {
+    cout << "Something is very wrong" << endl;
+  }
+  cout << dec;
 }
